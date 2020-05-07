d56eccb37894d72125bf87ee8f88572c575e9007
Jacqueline777 <jaczeboso@gmail.com>
2020-05-07 10:43:21 +0300

    Added

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
+
+import './index.css';
+
+
+
+
+
+ReactDOM.render(<h1>Helloo World</h1>, document.getElementById('root'));
+    
+
+serviceWorker.unregister();
