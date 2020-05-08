94f98ba3983ef3ca9fbbdfb1be66194830742d88
Harrison Ndegwa <harrisonndegwa65@gmail.com>
2020-05-08 15:27:22 +0300

    Added font-awesome

--- src/components/App/App.spec.js
+++ src/components/App/App.spec.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { shallow } from "enzyme";
 
+import App from "../App/App";
+import SideBar from "../SideBar/SideBar";
+import Main from "../Main/Main";
+
 describe('App',()=>{
 
     let wrapper;
 
     beforeEach(() => wrapper = shallow(<App/>));
 
-    it("Should render Header component",()=>{
-        expect(wrapper.containsMatchingElement(<Header/>)).toEqual(true);
+    it("Should render SideBar component",()=>{
+        expect(wrapper.containsMatchingElement(<SideBar/>)).toEqual(true);
     })
 
     it("Should render Main Component",()=>{
