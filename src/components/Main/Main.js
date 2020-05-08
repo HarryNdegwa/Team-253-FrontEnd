175b5af2e9edc8fadba0ed5d03931cfef65d515f
Harrison Ndegwa <harrisonndegwa65@gmail.com>
2020-05-08 17:52:23 +0300

    Added Hero component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
 
+import Hero from "../Hero/Hero";
+
+
 function Main(){
     return (
-        <main className="main col-md-7 mx-auto my-5 bg-dark">
-            
+        <main className="main col-md-7 mx-auto my-5">
+            <Hero/>
         </main>
     )
 }
